cdc148a60bafda5a0bf3b5af6ffcfeeee4610b9e
coffeenotfound <code.katzer@gmail.com>
2019-08-23 00:53:11 +0200

    Add (partially non working) code to interpreter and thread list

--- src/interpreter/funge_interpreter.rs
+++ src/interpreter/funge_interpreter.rs
@@ -1,32 +1,73 @@
 use crate::vector::Vector3;
 use crate::io::{CodeSource, CodeBuffer};
-use crate::interpreter::{FungeAddress, FungeSpace, FungeDim2, SpaceAccessorDim2};
+use crate::interpreter::{FungeAddress, FungeSpace, FungeDim2, SpaceAccessorDim2, ThreadList};
 use std::marker::PhantomData;
+use std::num::Wrapping;
 
 /// Interpreter for funge*.
 /// Instances directly contain the interpretation state.
 pub struct FungeInterpreter<'s> {
 	code_source: CodeSource,
-	threads: Vec<FungeThread<'s>>,
+	threads: ThreadList<'s>,
 	
 	funge_space: FungeSpace<'s, FungeDim2, i32, SpaceAccessorDim2<i32>>,
 }
 impl<'s> FungeInterpreter<'s> {
-	pub fn execute_next_tick(&mut self) {
+	pub fn new(code_source: CodeSource) -> Self {
+		// Instantiate
+		let mut interpreter = FungeInterpreter {
+			code_source,
+			threads: ThreadList::new(),
+			funge_space: FungeSpace::new(),
+		};
 		
-	}
-	
-	pub fn execute_thread_tick(&mut self, thread: &mut FungeThread) {
+		// Create initial thread
+		interpreter.create_thread(Vector3::new(), Vector3::new_xyz(1, 0, 0));
 		
+		return interpreter;
 	}
 	
-	pub fn load_code(&mut self, code: &CodeBuffer) {
-		//
+	/// Starts the execution of this interpreter by transferring
+	/// control over to it's main loop.
+	/// This method does not return aslong as the interpreter
+	/// is running.
+	pub fn start_execution(&mut self) {
+		// Main execution loop
+		loop {
+			// Execute next tick with each thread
+			let mut i = 0;
+			loop {
+				let mut thread = self.threads.get_mut(i).unwrap();
+				
+				// Run thread tick
+				self.execute_thread_tick(thread);
+				
+				// Increment index
+				i += 1;
+			}
+		}
 	}
 	
-	pub fn start_thread(&mut self, ip: InstructionPointer, delta: InstructionDelta) {
-		let thread = FungeThread::new(ip, delta);
-		self.threads.push(thread);
+	#[inline]
+	fn execute_thread_tick(&mut self, thread: &mut FungeThread) {
+		let ip = thread.ip;
+		
+		// Read instruction cell
+		let instruction = self.funge_space.read_cell(&ip);
+		
+		// Match instruction
+		
+		// Move ip
+//		thread.ip
+	}
+	
+	pub fn load_initial_code(&mut self, code: &CodeBuffer) {
+		// 
+	}
+	
+	pub fn create_thread(&mut self, ip: InstructionPointer, delta: InstructionDelta) {
+//		let thread = FungeThread::new(ip, delta);
+//		self.threads.push(thread);
 	}
 }
 
@@ -52,4 +93,12 @@ impl<'s> FungeThread<'s> {
 }
 
 pub type InstructionPointer = FungeAddress;
+impl InstructionPointer {
+	pub fn add_delta_wrapping(&mut self, delta: InstructionDelta) {
+		self.elements[0] = (Wrapping(self.x()) + Wrapping(delta.x())).0;
+		self.elements[1] = (Wrapping(self.y()) + Wrapping(delta.y())).0;
+		self.elements[2] = (Wrapping(self.z()) + Wrapping(delta.z())).0;
+	}
+}
+
 pub type InstructionDelta = Vector3<i32>;
--- src/interpreter/thread_list.rs
+++ src/interpreter/thread_list.rs
@@ -1,9 +1,35 @@
 use crate::interpreter::FungeThread;
 
-/// A list of funge threads with the appropriate manipulation methods.
+/// A list of funge threads kept in the right order.
 pub struct ThreadList<'s> {
 	threads: Vec<FungeThread<'s>>,
 }
 impl<'s> ThreadList<'s> {
+	pub fn new() -> Self {
+		ThreadList {
+			threads: Vec::with_capacity(8),
+		}
+	}
 	
+	pub fn get_mut(&mut self, index: u32) -> Option<&mut FungeThread<'s>> {
+		return self.threads.get_mut(index as usize);
+	}
+	
+	pub fn get(&self, index: u32) -> Option<&FungeThread<'s>> {
+		return self.threads.get(index as usize);
+	}
 }
+
+//impl<'s> std::ops::Index<usize> for ThreadList<'s> {
+//	type Output = ThreadList<'s>;
+//	
+//	fn index(&self, index: usize) -> &Self::Output {
+//		
+//	}
+//}
+//
+//impl<'s> std::ops::IndexMut<usize> for ThreadList<'s> {
+//	fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+//		
+//	}
+//}
