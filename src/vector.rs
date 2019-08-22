2d85d583d5a6833a6ba744ed519eadce7db4c76d
coffeenotfound <code.katzer@gmail.com>
2019-08-23 00:44:24 +0200

    Add new_xyz function to Vector3

--- src/vector.rs
+++ src/vector.rs
@@ -20,6 +20,12 @@ impl<T: Copy + Default> Vector3<T> {
 		}
 	}
 	
+	pub fn new_xyz(x: T, y: T, z: T) -> Self {
+		Vector3 {
+			elements: [x, y, z],
+		}
+	}
+	
 	#[inline(always)]
 	pub fn x(&self) -> T {
 		self.elements[0]
