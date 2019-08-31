e374d77e27d242581a67b0dc7774deba4482e371
coffeenotfound <code.katzer@gmail.com>
2019-08-31 16:29:27 +0200

    Add add_wrapping method to FungeAddress

--- src/interpreter/funge_address.rs
+++ src/interpreter/funge_address.rs
@@ -1,7 +1,16 @@
 use std::hash;
 use crate::vector::Vector3;
+use std::num::Wrapping;
 
 pub type FungeAddress = Vector3<i32>;
+
+impl FungeAddress {
+	pub fn add_wrapping(&mut self, other: &FungeAddress) {
+		self.elements[0] = (Wrapping(self.x()) + Wrapping(other.x())).0;
+		self.elements[1] = (Wrapping(self.y()) + Wrapping(other.y())).0;
+		self.elements[2] = (Wrapping(self.z()) + Wrapping(other.z())).0;
+	}
+}
 
 /// Hash impl for InstructionPointer
 impl hash::Hash for FungeAddress {
