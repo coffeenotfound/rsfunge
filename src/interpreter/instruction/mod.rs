00dd667343ad1e8ddef261bae150a8482cfee17b
coffeenotfound <code.katzer@gmail.com>
2019-09-06 21:05:48 +0200

    Remove old alphabet instruction files

--- src/interpreter/instruction/mod.rs
+++ src/interpreter/instruction/mod.rs
@@ -1,9 +1,2 @@
-mod instruction_table;
-mod instruction_alphabet;
-mod alphabet_registry;
-
-pub use instruction_table::*;
-pub use instruction_alphabet::*;
-pub use alphabet_registry::*;
 
 pub mod insts;
