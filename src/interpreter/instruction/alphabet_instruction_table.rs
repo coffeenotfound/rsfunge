85c6406ae6c93075abc1f4c06db2d7a2d2f0ed4f
coffeenotfound <code.katzer@gmail.com>
2019-09-09 18:08:24 +0200

    Add AlphabetInstructionTable stubs

---
+++ src/interpreter/instruction/alphabet_instruction_table.rs
@@ -0,0 +1,12 @@
+use crate::interpreter::fingerprint::Fingerprint;
+
+pub struct AlphabetInstructionTable {
+	
+}
+
+impl AlphabetInstructionTable {
+	
+	pub fn push_fingerprint(&mut self, fingerprint: &Fingerprint) {
+		
+	}
+}
--- src/interpreter/instruction/mod.rs
+++ src/interpreter/instruction/mod.rs
@@ -1,2 +1,6 @@
 
 pub mod insts;
+
+mod alphabet_instruction_table;
+
+pub use alphabet_instruction_table::*;
