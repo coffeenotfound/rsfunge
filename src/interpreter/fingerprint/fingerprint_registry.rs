86c5f4ae7040c7e745be70524b6c68622abc3681
coffeenotfound <code.katzer@gmail.com>
2019-09-17 17:16:36 +0200

    Finalize FingerprintRegistry implementation

--- src/interpreter/fingerprint/fingerprint_registry.rs
+++ src/interpreter/fingerprint/fingerprint_registry.rs
@@ -1,17 +1,24 @@
 use crate::interpreter::fingerprint::Fingerprint;
 use std::collections::HashMap;
+use std::rc::Rc;
 
-pub struct FingerprintRegistry {
-	fingerprint_map: HashMap<u32, Fingerprint>,
+pub struct FingerprintRegistry<'f> {
+	fingerprint_map: HashMap<u32, Rc<dyn Fingerprint<'f>>>,
 }
 
-impl FingerprintRegistry {
-	pub fn register_fingerprint(&mut self, fingerprint: Fingerprint) {
-		// Put into registry map
-		self.fingerprint_map.insert(fingerprint.get_id(), fingerprint);
+impl<'f> FingerprintRegistry<'f> {
+	pub fn new() -> Self {
+		FingerprintRegistry {
+			fingerprint_map: HashMap::new(),
+		}
 	}
 	
-	pub fn find_fingerprint(&mut self, id: u32) -> Option<&Fingerprint> {
-		return self.fingerprint_map.get(&id);
+	pub fn register_fingerprint(&mut self, fingerprint: Rc<dyn Fingerprint<'f>>) {
+		// Put into registry map
+		self.fingerprint_map.insert(fingerprint.get_name().get_fid(), fingerprint);
+	}
+	
+	pub fn find_fingerprint(&mut self, fid: u32) -> Option<&Rc<dyn Fingerprint<'f>>> {
+		return self.fingerprint_map.get(&fid);
 	}
 }
